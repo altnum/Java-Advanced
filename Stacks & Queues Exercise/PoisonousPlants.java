4de6acdbe37811a3d0770f4c036a011e89767f6d
Rado Popov <altnum16@gmail.com>
2020-09-21 20:44:01 +0300

    Poisonous Plants

--- Stacks & Queues Exercise/PoisonousPlants.java
+++ Stacks & Queues Exercise/PoisonousPlants.java
@@ -1,7 +1,47 @@
-import java.util.Scanner;
+import com.sun.jdi.ArrayReference;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class PoisonousPlants {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader buffer=new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.valueOf(buffer.readLine());
+
+        String[] plants = buffer.readLine().split(" ");
+
+        ArrayDeque<Integer> indexes = new ArrayDeque<>();
+        indexes.push(0);
+
+        int[] days = new int[n];
+
+        for (int i = 0; i < n; i++) {
+
+            int maxDays = 0;
+
+            while (indexes.size() > 0 && Integer.parseInt(plants[indexes.peek()]) >= Integer.parseInt(plants[i])) {
+                maxDays = Math.max(maxDays, days[indexes.pop()]);
+            }
+
+            if (indexes.size() > 0) {
+                days[i] = maxDays + 1;
+            }
+
+            indexes.push(i);
+        }
+        System.out.println(max(days));
+    }
+
+    private static int max(int[] days) {
+        int max = Integer.MIN_VALUE;
+        for (int day : days) {
+            if (day > max) {
+                max = day;
+            }
+        }
+        return max;
     }
 }
