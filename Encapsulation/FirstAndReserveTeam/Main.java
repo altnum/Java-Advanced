ea0f5267678c07fc36d28060dfd39217d31559d6
Rado Popov <altnum16@gmail.com>
2020-11-04 00:04:58 +0200

    Encapsulation1

---
+++ Encapsulation/FirstAndReserveTeam/Main.java
@@ -0,0 +1,25 @@
+package FirstAndReserveTeam;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        Team team = new Team("lit");
+
+        for (int i = 0; i < n; i++) {
+            String[] tokens = scan.nextLine().split("\\s+");
+
+            try {
+                Person person = new Person(tokens[0], tokens[1], Integer.parseInt(tokens[2]), Double.parseDouble(tokens[3]));
+                team.addPlayer(person);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        System.out.println("First team have " + team.getFirstTeam().size() + " players");
+        System.out.println("Reserve team have " + team.getReserveTeam().size() + " players");
+    }
+}
